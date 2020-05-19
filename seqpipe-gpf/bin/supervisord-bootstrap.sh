4769ac94950d31ad9558f65005d0c0efbefe94cd
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2020-05-19 14:40:25 +0300

    gpf gunicorn process renamed to 'gpf'...

--- seqpipe-gpf/bin/supervisord-bootstrap.sh
+++ seqpipe-gpf/bin/supervisord-bootstrap.sh
@@ -5,14 +5,14 @@ set -e
 
 /code/wdae/wdae/wdaemanage.py migrate
 
-supervisorctl start gpf19
+supervisorctl start gpf
 
 /wait-for-it.sh localhost:9001 -t 240
 
 rc=$?
 if [ $rc -ne 0 ]; then
     echo -e "\n---------------------------------------"
-    echo -e "  gpf19 gunicorn not ready! Exiting..."
+    echo -e "  gpf gunicorn not ready! Exiting..."
     echo -e "---------------------------------------"
     exit 1
 fi
