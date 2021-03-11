d38652bb69f2cc86c7ab627958728ec6c497ede4
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-03-11 18:37:53 +0200

    GPF-420 trying to debug jenkins build

--- seqpipe-gpfjs/package_gpfjs.sh
+++ seqpipe-gpfjs/package_gpfjs.sh
@@ -12,14 +12,26 @@ fi
 
 cd /work/gpfjs
 
+ls -l .
+
+
+echo "[package_gpfjs] going to remove node_modules..."
 rm -rf node_modules package-lock.json
+
+echo "[package_gpfjs] going to run npm install..."
 npm install
 
+echo "[package_gpfjs] going to remove dist..."
 rm -rf dist 
+
+
+echo "[package_gpfjs] going to build gpfjs..."
 ng build --prod --aot --configuration 'default' --base-href '/gpf_prefix/' --deploy-url '/gpf_prefix/'
 
+echo "[package_gpfjs] going to run ppindex.py..."
 python ppindex.py
 
+echo "[package_gpfjs] going to tar the distribution..."
 cd /work/gpfjs/dist/gpfjs && \
     tar zcvf /work/gpfjs-dist-default-${TAG}.tar.gz . &&\
     cd /work/gpfjs 
